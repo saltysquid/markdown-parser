61d5f401cfc72d9255baa7dbacf8520772001b08
Brian Legg <brianlegg99@gmail.com>
2021-02-22 17:51:52 -0500

    testing CI

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import ReactMarkdown from "react-markdown";
 
 function App() {
-  return <h2>markdown preview starter!!!</h2>;
+  return <h2>markdown preview starter</h2>;
 }
 
 export default App;
